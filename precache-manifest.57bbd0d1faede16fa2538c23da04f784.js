2e980243c8b414f56f261e97bb95b5f52557176a
darthbel <febelgine@gmail.com>
2019-06-22 20:26:20 -0400

    Updates

---
+++ precache-manifest.57bbd0d1faede16fa2538c23da04f784.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "080f71a52eec830b92ae8c9668e09dc2",
+    "url": "/front-end-developer-coding-test/index.html"
+  },
+  {
+    "revision": "6f867de5578d5b77a713",
+    "url": "/front-end-developer-coding-test/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "912535e82200a55f72a4",
+    "url": "/front-end-developer-coding-test/static/css/main.948d23a1.chunk.css"
+  },
+  {
+    "revision": "6f867de5578d5b77a713",
+    "url": "/front-end-developer-coding-test/static/js/2.d405bd03.chunk.js"
+  },
+  {
+    "revision": "912535e82200a55f72a4",
+    "url": "/front-end-developer-coding-test/static/js/main.7d2042c7.chunk.js"
+  },
+  {
+    "revision": "6e8345ab9e46320ce814",
+    "url": "/front-end-developer-coding-test/static/js/runtime~main.464a253e.js"
+  },
+  {
+    "revision": "099f02baaf9b43c6106e41df6c975fe8",
+    "url": "/front-end-developer-coding-test/static/media/rest_banner.099f02ba.jpg"
+  }
+]);
